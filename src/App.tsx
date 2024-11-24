9cc6c5ff097a1b7a8fa672eb3f1c1bb4e2547c06
fehmisener <fehmiisener@gmail.com>
2024-11-24 09:12:31 +0100

    style: change background style

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,32 @@ import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[url('https://gartic.io/static/images/new/textura.png')] bg-repeat">
-      <div className="absolute inset-0 bg-gradient-to-br from-purple-500/30 to-pink-500/30 backdrop-blur-sm" />
-      <LandingPage />
+<div className="min-h-screen relative bg-gradient-to-br from-purple-500/30 to-pink-500/30 ">
+    {/* Background container */}
+    <div
+        className="absolute inset-0"
+        style={{
+            backgroundImage:
+                "url('https://gartic.io/static/images/new/textura.png')",
+            backgroundRepeat: "repeat",
+            transformOrigin: "center",
+            width: "200%",
+            height: "200%",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%) rotate(345deg)",
+            opacity: 0.5, // Optional: adjust opacity as needed
+        }}
+    ></div>
+
+    {/* Content container */}
+    <div className="relative z-10">
+        <LandingPage />
     </div>
+</div>
   );
 }
+
+
 
 export default App;
