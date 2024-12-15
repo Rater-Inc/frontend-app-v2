f4384e249349df3ba8794cce522ab95946320c86
Melih <58309701+Sirmelihy@users.noreply.github.com>
2024-12-16 01:04:01 +0300

    feat : add redirect space to select-action

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CreateSpacePage from "./pages/CreateSpacePage";
 import SelectionPage from "./pages/SelectionPage";
@@ -11,74 +17,74 @@ import EnterSpaceIdPage from "./pages/EnterSpaceIdPage";
 
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
-function App() {
-    return (
-        <div className="min-h-screen relative bg-gradient-to-br from-purple-500/30 to-pink-500/30 ">
-            {/* Background container */}
-            <div
-                className="absolute inset-0"
-                style={{
-                    backgroundImage:
-                        "url('https://gartic.io/static/images/new/textura.png')",
-                    backgroundRepeat: "repeat",
-                    opacity: 0.7,
-                }}
-            ></div>
+const RedirectToSelectAction = () => {
+  const { spaceId } = useParams();
+  return <Navigate to={`/space/${spaceId}/select-action`} />;
+};
 
-            {/* Content container */}
-            <div className="relative z-10">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route
-                            path="/create-space"
-                            element={<CreateSpacePage />}
-                        />
-                        <Route
-                            path="/enter-space"
-                            element={<EnterSpaceIdPage />}
-                        />
-                        <Route
-                            path="/join/:spaceId"
-                            element={<JoinSpacePage />}
-                        />
-                        <Route
-                            path="/space/:spaceId/select-action"
-                            element={
-                                <ProtectedRoute>
-                                    <SelectionPage />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/space/:spaceId/rate"
-                            element={
-                                <ProtectedRoute>
-                                    <RatingPage />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/space/:spaceId/results/overall"
-                            element={
-                                <ProtectedRoute>
-                                    <OverallResultsPage />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/space/:spaceId/results/individual"
-                            element={
-                                <ProtectedRoute>
-                                    <IndividualResultsPage />
-                                </ProtectedRoute>
-                            }
-                        />
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        </div>
-    );
+function App() {
+  return (
+    <div className="min-h-screen relative bg-gradient-to-br from-purple-500/30 to-pink-500/30 ">
+      {/* Background container */}
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage:
+            "url('https://gartic.io/static/images/new/textura.png')",
+          backgroundRepeat: "repeat",
+          opacity: 0.7,
+        }}
+      ></div>
+
+      {/* Content container */}
+      <div className="relative z-10">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/create-space" element={<CreateSpacePage />} />
+            <Route path="/enter-space" element={<EnterSpaceIdPage />} />
+            <Route path="/join/:spaceId" element={<JoinSpacePage />} />
+            <Route
+              path="/space/:spaceId"
+              element={<RedirectToSelectAction />}
+            />
+            <Route
+              path="/space/:spaceId/select-action"
+              element={
+                <ProtectedRoute>
+                  <SelectionPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/space/:spaceId/rate"
+              element={
+                <ProtectedRoute>
+                  <RatingPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/space/:spaceId/results/overall"
+              element={
+                <ProtectedRoute>
+                  <OverallResultsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/space/:spaceId/results/individual"
+              element={
+                <ProtectedRoute>
+                  <IndividualResultsPage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
