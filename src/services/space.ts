a56926fb35b708701e226742a806e8a579b49185
Melih <58309701+Sirmelihy@users.noreply.github.com>
2025-02-07 00:41:05 +0300

    fix : return types

--- src/services/space.ts
+++ src/services/space.ts
@@ -1,14 +1,13 @@
-import { SpacexData, Rating , SpaceResultData } from '../types/types';
+import { PersistedSpaceData, Rating , SpaceResultData } from '../types/types';
 import { api, submitRatings } from './api';
 import { storage } from './storage';
 
 export const space = {
-  getSpace: async (spaceId: string, token: string): Promise<SpacexData> => {
-    try {
+  getSpace: async (spaceId: string, token: string): Promise<PersistedSpaceData> => {
       const spaceInfo = storage.getSpaceInfo();
       const spaceIdInfo = spaceInfo[spaceId]?.spaceData;
       if (spaceIdInfo) {
-        return spaceIdInfo as SpacexData;
+        return spaceIdInfo as PersistedSpaceData;
       }
 
       const response = await api.getSpace(spaceId, token);
@@ -20,13 +19,9 @@ export const space = {
           RatingData: spaceInfo[spaceId]?.RatingData || null
         };
         localStorage.setItem('space-info', JSON.stringify(spaceInfo));
-        return spaceData as SpacexData;
+        return spaceData as PersistedSpaceData;
       }
       throw new Error('Invalid space');
-    } catch (error) {
-      console.error('Error getting space:', error);
-      return {} as SpacexData;
-    }
   },
 
   submitRating: async (rating : Rating , token:string , spaceLink : string) => {
@@ -37,18 +32,14 @@ export const space = {
       localStorage.setItem("space-info", JSON.stringify(spaceInfo));
       return response.data;
     }
-    return null;
+    throw new Error("Error submitting rating");
   },
 
   getSpaceResult: async (spaceId:string,token:string): Promise<SpaceResultData | undefined> => {
-    try {
       const spaceResult = await api.getSpaceResult(spaceId, token);
       if (spaceResult?.status === 200) {
         return spaceResult.data as SpaceResultData;
       }
-    } catch (error) {
-      console.error('Error getting space result:', error);
-    }
-    return undefined;
+      throw new Error('Invalid space result');
   }
 };
